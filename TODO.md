04331c3d7b964ee6601f963c33e543d6e323929b
Max Drach <mdrach@stanford.edu>
2018-03-24 15:31:09 -0700

    Blank pages and routing between them

---
+++ TODO.md
@@ -0,0 +1,10 @@
+
+# Things that may need to be done in the future
+- Note that [you may need to configure your production server to support client-side routing](#serving-apps-with-client-side-routing) before deploying your app.
+
+
+# Conceptual things to look up later
+- Why does `export default name1` in ./file get imported fine in ./anotherfile with `import name2 from ./file`
+
+
+
--- src/App.js
+++ src/App.js
@@ -1,21 +1,100 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/bit_operations">Bit Operations</Link>
+        </li>
+        <li>
+          <Link to="/floating_point">Floating Point Numbers</Link>
+        </li>
+        <li>
+          <Link to="/c_strings">C Strings</Link>
+        </li>
+        <li>
+          <Link to="/assembly">Assembly</Link>
+        </li>
+        <li>
+          <Link to="/heap_allocator">Heap Allocator</Link>
+        </li>
+      </ul>
 
-export default App;
+      <hr />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/bit_operations" component={BitOperations} />
+      <Route path="/floating_point" component={FloatingPointNumbers} />
+      <Route path="/c_strings" component={CStrings} />
+      <Route path="/assembly" component={Assembly} />
+      <Route path="/heap_allocator" component={HeapAllocator} />
+    </div>
+  </Router>
+);
+
+const Home = () => (
+  <div>
+    <h2>Welcome to CS107 Apps, a page with helpful visualizations for CS107 students</h2>
+  </div>
+);
+
+const BitOperations = () => (
+  <div>
+    <h2>Bit Operations</h2>
+  </div>
+);
+
+const FloatingPointNumbers = () => (
+  <div>
+    <h2>Floating Point Numbers</h2>
+  </div>
+);
+
+const CStrings = () => (
+  <div>
+    <h2>C Strings</h2>
+  </div>
+);
+
+const Assembly = () => (
+  <div>
+    <h2>Assembly</h2>
+  </div>
+);
+
+const HeapAllocator = ({ match }) => (
+  <div>
+    <h2>Heap Allocator</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic} />
+    <Route
+      exact
+      path={match.url}
+      render={() => <h3>Please select a topic.</h3>}
+    />
+  </div>
+);
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+);
+
+export default BasicExample;
